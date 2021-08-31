650f7c57329e36b2c377432c795a329cebcc39c9
joalavedra <62625514+joalavedra@users.noreply.github.com>
2021-08-31 18:10:03 +0200

    Update landing-page.js

--- src/views/landing-page.js
+++ src/views/landing-page.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { Helmet } from 'react-helmet'
-import React, { Component } from "react";
 import referlist from "referlist";
 
 import OutlineButtonColour from '../components/outline-button-colour'
